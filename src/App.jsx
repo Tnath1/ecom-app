0949a4314e068122fb3c5897c3b3f538856743cb
Arome Jonathan Ukpoju <99257453+Tnath1@users.noreply.github.com>
2024-06-03 02:57:59 +0100

    working on Landing

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import Hero from "./components/Hero";
 import BestSeller from "./components/BestSeller";
 import Footer from "./components/Footer";
+import CallToAction from "./components/CallToAction";
 
 function App() {
   return (
     <div>
       <Hero />
       <BestSeller />
+      <CallToAction/>
       <Footer/>
     </div>
   );
---
+++ src/components/CallToAction.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "../components/css/callToAction.css";
+
+const CallToAction = () => {
+  return (
+    <>
+      <div className="txt-wapper">
+        <div className=" txt-containe">
+          <div>
+            <div className="flex">
+              <h5>Designing Better Experience</h5>
+            </div>
+            <div className="flex">
+              <h3>Problems trying to resolve the conflict between </h3>
+            </div>
+
+            <div className="flex">
+              <h6>
+                Problems trying to resolve the conflict between the two major
+                realms of Classical physics:
+              </h6>
+            </div>
+            <div className="flex">
+              <p className="num">$16.48</p>
+            </div>
+            <div className="flex">
+              <button>ADD YOUR CALL TO ACTION</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CallToAction;
